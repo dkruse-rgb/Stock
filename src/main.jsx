025e4b31c03d660ec50254bb877e44b7aae77980
dkruse-rgb <dkruse1027@gmail.com>
2026-04-30 20:18:49 -0500

    Use Replay Mode app

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import TradeTrainer from './TradeTrainer.jsx';
+import TradeTrainerReplay from './TradeTrainerReplay.jsx';
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <TradeTrainer />
+    <TradeTrainerReplay />
   </React.StrictMode>,
 );
